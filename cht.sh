95953835d7a2fb0a49a438eb0234425dd5043cd5
rosascript <rosascript@gmail.com>
2021-11-27 01:48:25 -0500

    feat: add cht.sh for easy cheat-sheet accessibility

---
+++ cht.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# hey vi, when you get on linux, and setup tmux, add the line below to your tmux.conf for a tmux keybind for this
+# bind-key -r <key> run-shell "tmux neww ~/.dotfiles/cht.sh"
+
+languages=`echo "golang lua cpp c typescript nodejs rust" \ tr ' ' '\n'`
+core_utils=`echo "xargs find mv sed awk" | tr ' ' '\n'`
+
+selected=`printf "$languages\n$core_utils" | fzf`
+read -p "query: " query
+
+if printf $languages | grep -qs $selected; then
+	tmux neww curl cht.sh/$selected/`echo $query | tr ' ' '+'` & while [ : ]; do sleep 1; done
+else
+	tmux neww curl cht.sh/$selected~$query & while [ : ]; do sleep 1; done
+fi
+
