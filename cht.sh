889f4a733c5370a9055b198389d8a08844dfb290
rosascript <rosascript@gmail.com>
2021-11-27 03:23:07 -0500

    feat: add naming to tmux windows

--- cht.sh
+++ cht.sh
@@ -10,8 +10,8 @@ selected=`printf "$languages\n$core_utils" | fzf`
 read -p "query: " query
 
 if printf $languages | grep -qs $selected; then
-	tmux neww curl cht.sh/$selected/`echo $query | tr ' ' '+'` & while [ : ]; do sleep 1; done
+	tmux neww -n "$selected/$query" curl cht.sh/$selected/`echo $query | tr ' ' '+'` & while [ : ]; do sleep 1; done
 else
-	tmux neww curl cht.sh/$selected~$query & while [ : ]; do sleep 1; done
+	tmux neww -n "$selected~$query" curl cht.sh/$selected~$query & while [ : ]; do sleep 1; done
 fi
 
